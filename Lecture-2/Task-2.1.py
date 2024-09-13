64cab140ce76b2cabb95c44161a7c375237e89f7
tornike-ch <thornike.chikhladze@gmail.com>
2024-09-13 21:12:46 +0400

    Second Homework

--- Lecture-2/Task-2.1.py
+++ Lecture-2/Task-2.1.py
@@ -1,1 +1,14 @@
-print("Test Test")
+"""
+1. Დაწერეთ პროგრამა რომელიც მიიღებს true ან false. Თუ პროგრამამ მიიღო true, ეკრანზე დაბეჭდეთ “whoala”. 
+"""
+
+true_or_false = input("დაწერე true ან false: ")
+
+if true_or_false == "true" or true_or_false == "false":
+    if true_or_false == "true":
+        print("whoala")
+    else:
+        exit
+else:
+    print("პასუხი არ შეესაბამება მოთხოვნას")
+    exit(1)
